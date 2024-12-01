488c598870cde7d767aff5612935c99c7448464a
REWraith <wraithre@outlook.com>
2024-12-01 17:11:39 +0300

    Düzenlemeler.

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    images: {
-      domains: ['i.imgur.com', 'imgur.com'], 
-    },
-  };
-  
-  module.exports = nextConfig;
-  
+  images: {
+    domains: ['i.imgur.com', 'imgur.com'],
+    unoptimized: true, // Disable Image Optimization for static exports
+  },
+  output: 'export', // Enable static export
+};
+
+module.exports = nextConfig;
