353190e625361de3dee3d3255afa0dab14fd9a09
REWraith <wraithre@outlook.com>
2024-12-01 17:15:21 +0300

    Title düzenleme

--- pages/_app.js
+++ pages/_app.js
@@ -9,7 +9,7 @@ export default function MyApp ({ Component, pageProps }) {
         <link rel='icon' href='/assets/minilogo.png' />
         <meta
           name='description'
-          content='Division, mobil uygulama geliştirme hizmetlerinde lider bir şirkettir.'
+          content='Pusula Alüminyum, yüksek kalite standartlarında alüminyum çözümleri sunan, müşteri odaklı yaklaşımıyla sektöründe fark yaratan bir firmadır. Ürün yelpazesi, yapı ve endüstri projelerine özel olarak tasarlanmış dayanıklı, hafif ve estetik alüminyum profillerden oluşmaktadır. Teknolojik yenilikleri takip eden ve sürekli gelişimi hedefleyen firma, modern üretim tesisleri ve uzman kadrosu ile müşterilerine hem standart hem de özel üretim seçenekleri sunmaktadır. Çevreye duyarlılığı ve sürdürülebilirlik ilkelerini benimseyen Pusula Alüminyum, uzun ömürlü ve geri dönüştürülebilir ürünleriyle sektördeki lider konumunu güçlendirmektedir.'
         />
         <link rel='icon' href='/images/footerlogo.png' type='image/png' />
       </Head>
